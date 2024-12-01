5fa66bc20e8e5d7ce1ed7e01f4723e0240488ed7
Jude Stevens <stevensjude@icloud.com>
2024-12-01 15:00:00 +0000

    Reformat
    recommit because I forgot to format the file properly

--- day_1/main.py
+++ day_1/main.py
@@ -1,15 +1,3 @@
-left  = []
-right = []
-
-with open("input.txt", "r") as f:
-    for line in f:
-        # Extract data from input.txt, cleanup, and cast to int for later use
-        data = list(map(int, line.replace("\n", "").split("   ")))
-        # split left and right side of data into seperate lists
-        left.append(data[0])
-        right.append(data[1])
-        
-
 def findDifference(left, right, tot = 0):
     # sort lists from smallest to largest
     left.sort()
@@ -34,6 +22,18 @@ def findSimilarity(left, right, tot = 0):
         tot = tot + (left[i] * c)
 
     return tot
+
+# Driver code
+left  = []
+right = []
+
+with open("input.txt", "r") as f:
+    for line in f:
+        # Extract data from input.txt, cleanup, and cast to int for later use
+        data = list(map(int, line.replace("\n", "").split("   ")))
+        # split left and right side of data into seperate lists
+        left.append(data[0])
+        right.append(data[1])
 
 print(f'Difference: {findDifference(left, right)}')
 print(f'Similarity: {findSimilarity(left, right)}')
